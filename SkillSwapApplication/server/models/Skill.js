f8e0d4df88afa0b03398095965d13f1ba5bf8d7e
Andreea Penea <andreea.penea@student.upt.ro>
2025-04-06 00:01:39 +0300

    deleted some dirs

--- SkillSwapApplication/server/models/Skill.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const SkillSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Skill', SkillSchema);
